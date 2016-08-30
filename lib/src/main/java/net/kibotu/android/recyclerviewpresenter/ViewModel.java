f798c34df4801d81896cb3c2608f39af48c48e21
Jan Rabe <jan.rabe@exozet.com>
2016-08-30 15:24:31 +0200

    updated gradle to 3.0 and added viewmodel class

---
+++ lib/src/main/java/net/kibotu/android/recyclerviewpresenter/ViewModel.java
@@ -0,0 +1,47 @@
+package net.kibotu.android.recyclerviewpresenter;
+
+/**
+ * Created by jan.rabe on 05/08/16.
+ */
+public class ViewModel<T> {
+
+    public T model;
+    public OnItemClickListener<ViewModel<T>> onItemClickListener;
+
+    public ViewModel<T> setModel(T model) {
+        this.model = model;
+        return this;
+    }
+
+    public ViewModel<T> setOnItemClickListener(OnItemClickListener<ViewModel<T>> onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ViewModel<?> viewModel = (ViewModel<?>) o;
+
+        if (model != null ? !model.equals(viewModel.model) : viewModel.model != null) return false;
+        return onItemClickListener != null ? onItemClickListener.equals(viewModel.onItemClickListener) : viewModel.onItemClickListener == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = model != null ? model.hashCode() : 0;
+        result = 31 * result + (onItemClickListener != null ? onItemClickListener.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ViewModel{" +
+                "model=" + model +
+                ", onItemClickListener=" + onItemClickListener +
+                '}';
+    }
+}
